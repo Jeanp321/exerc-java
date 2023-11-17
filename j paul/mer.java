0085eb8f6ee7dcffd5a2f055f58cbd24bfca10e4
Jeanp321 <151026822+Jeanp321@users.noreply.github.com>
2023-11-17 09:20:37 +0200

    Add files via upload

---
+++ j paul/mer.java
@@ -0,0 +1,9 @@
+class mer{
+    public static void main(String[] args) {
+        int[] oddNumbers = {1, 3, 5, 7};
+          System.out.println(oddNumbers[0]); // 1
+          System.out.println(oddNumbers[1]); // 3
+          System.out.println(oddNumbers[2]); // 5
+          System.out.println(oddNumbers[3]); // 7
+    }
+}
---
+++ j paul/teacher.java
@@ -0,0 +1,12 @@
+ class teacher{
+    public static void main(String args[]){
+   int num = 0;
+ System.out.println("Let's count to 10!");
+		while(num < 10)
+ {
+ num = num + 1;
+ System.out.println("Number: " + num);
+ }
+ System.out.println("We have counted to 10! Hurray! ");
+ }
+}
